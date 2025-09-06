8651d429792eed3e9705d66feeb3d7a52e9d8374
RajneeshKrSharma <rajneeshsharma199@gmail.com>
2025-09-06 21:53:32 +0530

    refactor(setting): set debug to false

--- schedify/settings.py
+++ schedify/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-kmfy0yzvl$ky!iw$z++e0$(r-4-xw(gk53)0glmbl+ngq%0h@i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['10.0.2.2', '127.0.0.1', 'schedify.pythonanywhere.com']
 
